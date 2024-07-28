22a53dec4d8f12ccb58271511141570bd6471d69
Luka Misic <lukamisic2001@gmail.com>
2024-07-28 13:37:18 +0200

    Napravljeni modeli i migracije

---
+++ turisticki-vodic/app/Models/Route.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Route extends Model
+{
+    protected $fillable = ['name', 'description', 'user_id', 'is_approved'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function locations()
+    {
+        return $this->hasMany(Location::class);
+    }
+}
